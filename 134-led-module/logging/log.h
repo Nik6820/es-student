032c822ed0f8308a063ebc1d31de69f7cf3a43e0
Никита Губарев <ngubarev@internet.ru>
2026-09-24 17:22:13 +0300

    Сделана п1.3.6 исправление 2

--- 134-led-module/logging/log.h
+++ 134-led-module/logging/log.h
@@ -33,7 +33,7 @@ void log_prefix(const char *level, const char *function, int line);
     {                                               \
         if (LOG_LEVEL >= LOG_LEVEL_ERR)             \
         {                                           \
-            log_prefix("inf", __func__, __LINE__);  \
+            log_prefix("err", __func__, __LINE__);  \
             printf(__VA_ARGS__);                    \
         }                                           \   
     } while (0)
@@ -43,7 +43,7 @@ void log_prefix(const char *level, const char *function, int line);
     {                                               \
         if (LOG_LEVEL >= LOG_LEVEL_DBG)             \
         {                                           \
-            log_prefix("inf", __func__, __LINE__);  \
+            log_prefix("dbg", __func__, __LINE__);  \
             printf(__VA_ARGS__);                    \
         }                                           \
     } while (0)
